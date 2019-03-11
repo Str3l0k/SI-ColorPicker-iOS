8ff6055a61b9ec5b98752d337c94c2968e97252b
SebastianBarz <SebastianBarzDev@gmail.com>
2019-03-11 18:28:15 +0100

    Initial workspace commit

---
+++ SIColorPicker App/SIColorPicker App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SIColorPicker App
+//
+//  Created by Sebastian on 11.03.19.
+//  Copyright © 2019 Sebastian Barz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SIColorPicker/SIColorPicker lib/SIColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  SIColorPicker.h
+//  SIColorPicker
+//
+//  Created by Sebastian on 10.03.19.
+//  Copyright © 2019 Sebastian Barz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SIColorPicker.
+FOUNDATION_EXPORT double SIColorPickerVersionNumber;
+
+//! Project version string for SIColorPicker.
+FOUNDATION_EXPORT const unsigned char SIColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SIColorPicker/PublicHeader.h>
+
+
